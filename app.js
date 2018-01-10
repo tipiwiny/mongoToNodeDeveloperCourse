788e090260e109f2ddcf79fa0bbb0301ac4f44f7
Juan Luis Perez <tipiwiny@gmail.com>
2018-01-10 21:51:02 +0100

    Connect to db

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var MongoClient = require('mongodb').MongoClient,
+const MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
-var url = 'mongodb://localhost:27017/video';
+const url = 'mongodb://localhost:27017/video';
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function(err, client) {
 
     assert.equal(null, err);
     console.log("Successfully connected to server");
-
+    const db = client.db('video');
     // Find some documents in our collection
     db.collection('movies').find({}).toArray(function(err, docs) {
 
@@ -17,11 +17,9 @@ MongoClient.connect(url, function(err, db) {
         });
 
         // Close the DB
-        db.close();
+        client.close();
     });
 
     // Declare success
     console.log("Called find()");
 });
-
-
