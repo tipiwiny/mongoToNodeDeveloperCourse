5847182243e30cf742bf58e01f5895deb194540d
Juan Luis Perez <tipiwiny@gmail.com>
2018-01-10 23:23:25 +0100

    template in express

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const engines = require('consolidate');
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+app.engine('html', engines.nunjucks)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+app.get('/', (req, res) =>{
+    res.render('hello',{ name: 'Templates'});
 });
 
-app.use(function(req, res){
+app.use((req, res) => {
     res.sendStatus(404);
 });
 
-const server = app.listen(3000, function() {
+const server = app.listen(3000, () =>{
     var port = server.address().port;
     console.log('Express server listening on port %s', port);
 });
