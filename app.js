ad3fec3ec039111333ab717f63532565a20eeed9
Juan Luis Perez <tipiwiny@gmail.com>
2018-01-10 23:06:34 +0100

    added express example

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
-const MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+const express = require('express');
+const app = express();
 
-const url = 'mongodb://localhost:27017';
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
 
-MongoClient.connect(url, function(err, client) {
+app.use(function(req, res){
+    res.sendStatus(404);
+});
 
-    assert.equal(null, err);
-    console.log("Successfully connected to server");
-    const db = client.db('video');
-    // Find some documents in our collection
-    db.collection('movies').find({}).toArray(function(err, docs) {
-
-        // Print the documents returned
-        docs.forEach(function(doc) {
-            console.log(doc.title);
-        });
-
-        // Close the DB
-        client.close();
-    });
-
-    // Declare success
-    console.log("Called find()");
+const server = app.listen(3000, function() {
+    var port = server.address().port;
+    console.log('Express server listening on port %s', port);
 });
