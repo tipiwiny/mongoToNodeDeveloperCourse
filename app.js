4eed2ebf8f271ea171bba7d5f93f7aaec32fed67
Juan Luis Perez <tipiwiny@gmail.com>
2018-01-11 23:12:33 +0100

    mongo + express

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const express = require('express');
-const app = express();
-const engines = require('consolidate');
+var express = require('express'),
+    app = express(),
+    engines = require('consolidate'),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 
-app.engine('html', engines.nunjucks)
-app.set('view engine', 'html')
-app.set('views', __dirname + '/views')
-app.get('/', (req, res) =>{
-    res.render('hello',{ name: 'Templates'});
-});
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
-app.use((req, res) => {
-    res.sendStatus(404);
-});
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
 
-const server = app.listen(3000, () =>{
-    var port = server.address().port;
-    console.log('Express server listening on port %s', port);
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
+    const db = client.db('video')
+    app.get('/', function(req, res){
+
+        db.collection('movies').find({}).toArray(function(err, docs) {
+            res.render('movies', { 'movies': docs } );
+        });
+
+    });
+
+    app.use(function(req, res){
+        res.sendStatus(404);
+    });
+
+    var server = app.listen(3000, function() {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
+
 });
